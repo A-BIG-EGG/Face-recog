67ad0f84553f25fa090a65f0b592d04a7c6c1432
A-BIG-EGG <jbohoslawec@gmail.com>
2021-03-22 15:18:59 +0000

    Replaced with squid suggested code

--- buttontest.py
+++ buttontest.py
@@ -1,8 +1,9 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 16 to be an input pin and set initial value to be pulled low (off)
+from button import *
+import time
 
-while True: # Run forever
-    if GPIO.input(16) == GPIO.HIGH:
-        print("Button was pushed!")
+
+b = Button(23, debounce=0.1)
+
+while True:
+    if b.is_pressed():
+        print(time.time())
