76d37b817df944678280de714293acced5490e92
A-BIG-EGG <jbohoslawec@gmail.com>
2021-03-22 22:09:59 +0000

    Tweaked lib import lines

--- epd_2in13_V2_test.py
+++ epd_2in13_V2_test.py
@@ -8,7 +8,7 @@ if os.path.exists(libdir):
     sys.path.append(libdir)
 
 import logging
-from waveshare_epd import epd2in13_V2
+from lib.waveshare_epd import epd2in13_V2
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
@@ -17,7 +17,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 try:
     logging.info("epd2in13_V2 Demo")
-    
+
     epd = epd2in13_V2.EPD()
     logging.info("init and Clear")
     epd.init(epd.FULL_UPDATE)
@@ -26,11 +26,11 @@ try:
     # Drawing on the image
     font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    
+
     logging.info("1.Drawing on the image...")
-    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
+    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(image)
-    
+
     draw.rectangle([(0,0),(50,50)],outline = 0)
     draw.rectangle([(55,0),(100,50)],fill = 0)
     draw.line([(0,0),(50,50)], fill = 0,width = 1)
@@ -45,30 +45,30 @@ try:
     draw.text((110, 90), u'微雪电子', font = font24, fill = 0)
     epd.display(epd.getbuffer(image))
     time.sleep(2)
-    
-    # read bmp file 
+
+    # read bmp file
     logging.info("2.read bmp file...")
     image = Image.open(os.path.join(picdir, '2in13.bmp'))
     epd.display(epd.getbuffer(image))
     time.sleep(2)
-    
+
     # read bmp file on window
     logging.info("3.read bmp file on window...")
     # epd.Clear(0xFF)
     image1 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
     bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
-    image1.paste(bmp, (2,2))    
+    image1.paste(bmp, (2,2))
     epd.display(epd.getbuffer(image1))
     time.sleep(2)
-    
+
     # # partial update
     logging.info("4.show time...")
     time_image = Image.new('1', (epd.height, epd.width), 255)
     time_draw = ImageDraw.Draw(time_image)
-    
+
     epd.init(epd.FULL_UPDATE)
     epd.displayPartBaseImage(epd.getbuffer(time_image))
-    
+
     epd.init(epd.PART_UPDATE)
     num = 0
     while (True):
@@ -82,14 +82,14 @@ try:
     logging.info("Clear...")
     epd.init(epd.FULL_UPDATE)
     epd.Clear(0xFF)
-    
+
     logging.info("Goto Sleep...")
     epd.sleep()
-        
+
 except IOError as e:
     logging.info(e)
-    
-except KeyboardInterrupt:    
+
+except KeyboardInterrupt:
     logging.info("ctrl + c:")
     epd2in13_V2.epdconfig.module_exit()
     exit()
