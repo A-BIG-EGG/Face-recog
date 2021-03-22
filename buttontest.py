a1ded614a6472a59b38837b21ef12bcc87b55f31
A-BIG-EGG <jbohoslawec@gmail.com>
2021-03-22 14:18:30 +0000

    Trying 28

--- buttontest.py
+++ buttontest.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 20 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(28, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 28 to be an input pin and set initial value to be pulled low (off)
 
 while True: # Run forever
-    if GPIO.input(20) == GPIO.HIGH:
+    if GPIO.input(28) == GPIO.HIGH:
         print("Button was pushed!")
