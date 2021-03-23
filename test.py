d2264c93f69b6b504761bde37e541f9fca24f310
A-BIG-EGG <jbohoslawec@gmail.com>
2021-03-23 11:15:53 +0000

    Attempting face detection using cv3

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 import cv2
 import sys
+import logging as log
+import datetime as dt
+from time import sleep
 
-cascPath = sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
+log.basicConfig(filename='webcam.log',level=log.INFO)
 
 video_capture = cv2.VideoCapture(0)
+anterior = 0
 
 while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
@@ -16,19 +26,27 @@ while True:
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        minSize=(30, 30)
     )
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
+
     # Display the resulting frame
     cv2.imshow('Video', frame)
 
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
 
 # When everything is done, release the capture
 video_capture.release()
