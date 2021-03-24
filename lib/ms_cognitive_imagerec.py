f5c5cba9413ccfa907f3c92444f11fa2dd3800f9
A-BIG-EGG <jbohoslawec@gmail.com>
2021-03-24 18:31:24 +0000

    Added hair to ms_cog

--- lib/ms_cognitive_imagerec.py
+++ lib/ms_cognitive_imagerec.py
@@ -74,7 +74,7 @@ def ms_WhoDoYouSee (body):
     params = {
         'returnFaceId': 'true',
        # 'returnFaceLandmarks': 'false',
-        'returnFaceAttributes': 'age,gender,smile,emotion,glasses',
+        'returnFaceAttributes': 'age,gender,smile,emotion,glasses,hair',
     }
 
     try:
