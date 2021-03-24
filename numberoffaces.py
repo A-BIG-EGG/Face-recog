31bc7149018fc80e66b61c913bec4033f0aa28cd
A-BIG-EGG <jbohoslawec@gmail.com>
2021-03-24 12:47:16 +0000

    Created numberoffaces.py- prints #faces in the frame whenever it changes

---
+++ numberoffaces.py
@@ -0,0 +1,40 @@
+import cv2
+import sys
+import logging as log
+import datetime as dt
+from time import sleep
+
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+log.basicConfig(filename='webcam.log',level=log.INFO)
+
+video_capture = cv2.VideoCapture(0)
+anterior = 0
+
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+
+    if anterior != len(faces):
+        anterior = len(faces)
+        print('Number of faces: '+str(len(faces)))
+
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
