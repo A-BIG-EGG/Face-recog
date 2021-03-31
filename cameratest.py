f60f02e3678edcd99abdcdb0fa99e21ee292c77a
A-BIG-EGG <jbohoslawec@gmail.com>
2021-03-31 12:42:12 +0100

    Added alert to display

--- cameratest.py
+++ cameratest.py
@@ -5,14 +5,27 @@ import logging as log
 import datetime as dt
 from time import sleep
 from lib.ms_cognitive_imagerec import ms_WhatDoYouSee, ms_WhoDoYouSee, ms_GetFaceAttribs, FaceAttribs
-from PIL import Image
+from PIL import Image,ImageDraw,ImageFont
 import requests
 from picamera import PiCamera
-import time
+from waveshare_epd import epd2in13_V2
+import traceback
 
+font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
+font40 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 40)
 camera = PiCamera() #Define camera (won't work on non-pi devices)
 camera.vflip = True #Rotate image by flipping v and h
 camera.hflip = True
+
+#Alert user to image being taken
+epd = epd2in13_V2.EPD()
+epd.init(epd.FULL_UPDATE)
+epd.Clear(0xFF)
+image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+draw = ImageDraw.Draw(image)
+draw.text((35, 45), 'Ready?', font = font40, fill = 0)
+image = image.transpose(Image.ROTATE_180) #rotates image?
+epd.display(epd.getbuffer(image))
 time.sleep(1)
 
 camera.capture("img.jpg") #Save image as .jpg
