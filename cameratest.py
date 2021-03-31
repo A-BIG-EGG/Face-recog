74ac4986315ed3455c0b78c7823555283dad9142
A-BIG-EGG <jbohoslawec@gmail.com>
2021-03-31 14:21:37 +0100

    Redefining face/response

--- cameratest.py
+++ cameratest.py
@@ -63,8 +63,9 @@ if len(faces) > 0: #Action if there are faces in the frame
     body = open('img.jpg','rb').read() #Read image in API call-firendly format
     try:
         response = ms_WhoDoYouSee(body) #Call API- see ms_cognitive_imagerec for details and options
-        time.sleep(0.2)
-        faceAttribs = ms_GetFaceAttribs (face)
+        for face in response:
+            n += 1
+            faceAttribs = ms_GetFaceAttribs (face)
     except Exception as e:
         print(e)
 else: #Action for no faces
